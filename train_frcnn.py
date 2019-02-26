5849900222f070deebe052b65bedef62f9155f1d
Samriddhi Sinha <samriddhidjokestersinha@gmail.com>
2019-02-26 07:27:31 +0530

    Update train_frcnn.py

--- train_frcnn.py
+++ train_frcnn.py
@@ -141,10 +141,8 @@ except:
 	print('Could not load pretrained model weights. Weights can be found in the keras application folder \
 		https://github.com/fchollet/keras/tree/master/keras/applications')
 
-optimizer = Adam(lr=1e-5)
-optimizer_classifier = Adam(lr=1e-5)
-model_rpn.compile(optimizer=optimizer, loss=[losses.rpn_loss_cls(num_anchors), losses.rpn_loss_regr(num_anchors)])
-model_classifier.compile(optimizer=optimizer_classifier, loss=[losses.class_loss_cls, losses.class_loss_regr(len(classes_count)-1)], metrics={'dense_class_{}'.format(len(classes_count)): 'accuracy'})
+model_rpn.compile(optimizer=Adam, loss=[losses.rpn_loss_cls(num_anchors), losses.rpn_loss_regr(num_anchors)])
+model_classifier.compile(optimizer=Adam, loss=[losses.class_loss_cls, losses.class_loss_regr(len(classes_count)-1)], metrics={'dense_class_{}'.format(len(classes_count)): 'accuracy'})
 model_all.compile(optimizer=sgd, loss= sum([losses.rpn_loss_cls(num_anchors), losses.rpn_loss_regr(num_anchors)]))
 
 epoch_length = 500
