074993640fd7304d2e0258cd5650d8bfe3fd24ea
Samriddhi Sinha <samriddhidjokestersinha@gmail.com>
2019-02-26 07:21:37 +0530

    Update train_frcnn.py

--- train_frcnn.py
+++ train_frcnn.py
@@ -144,8 +144,8 @@ except:
 optimizer = Adam(lr=1e-5)
 optimizer_classifier = Adam(lr=1e-5)
 model_rpn.compile(optimizer=optimizer, loss=[losses.rpn_loss_cls(num_anchors), losses.rpn_loss_regr(num_anchors)])
-#model_classifier.compile(optimizer=optimizer, loss=[losses.rpn_loss_cls(num_anchors), losses.rpn_loss_regr(num_anchors)], metrics={'dense_class_{}'.format(len(classes_count)): 'accuracy'})
-model_all.compile(optimizer=optimizer, loss= [losses.rpn_loss_cls(num_anchors), losses.rpn_loss_regr(num_anchors)])
+model_classifier.compile(optimizer=optimizer_classifier, loss=[losses.class_loss_cls, losses.class_loss_regr(len(classes_count)-1)], metrics={'dense_class_{}'.format(len(classes_count)): 'accuracy'})
+model_all.compile(optimizer=sgd, loss= sum([losses.rpn_loss_cls(num_anchors), losses.rpn_loss_regr(num_anchors)]))
 
 epoch_length = 500
 num_epochs = int(options.num_epochs)
